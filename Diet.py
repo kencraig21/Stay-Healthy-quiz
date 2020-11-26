4f72ef39db9af85c4d1d1c7991df3aa54ea8c971
kencraig21 <69943260+kencraig21@users.noreply.github.com>
2020-11-26 18:04:02 -0500

    Added Details to Sleep

---
+++ Diet.py
@@ -0,0 +1,9 @@
+question1 = [How many calories is recommened that we intake a day]/n"
+question2 = [How much water should we drink a day]/n"
+question3 = [Is white wine or red wine healthier]/n"
+question4 = [Added sugar is one of the unhealthiest aspects of a modern diet? T/F]/n"
+
+
+answer1 = 
+answer2 = 
+answer3 = 
